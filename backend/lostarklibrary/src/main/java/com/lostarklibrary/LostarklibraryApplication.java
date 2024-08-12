3d620849f865b325692e94180f993c7ffade7772
Junkai336 <gjwnssla2@gmail.com>
2024-08-13 01:07:58 +0900

    [Chore] 스프링부트 세팅 진행중

---
+++ backend/lostarklibrary/src/main/java/com/lostarklibrary/LostarklibraryApplication.java
@@ -0,0 +1,13 @@
+package com.lostarklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarklibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostarklibraryApplication.class, args);
+	}
+
+}
---
+++ backend/lostarklibrary/src/test/java/com/lostarklibrary/LostarklibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostarklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarklibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
